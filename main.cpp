9c69265275d4aea95078c2309a816f29c26ac677
BenCinn <pp.rongpan@gmail.com>
2022-07-27 21:15:27 +0700

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file main.cpp
+ * @author Chitsanupong Rongpan (pp.rongpan@gmail.com)
+ * @brief For my study
+ * 
+ */
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+  std::cout<<"Hello\n";
+  return 0;
+}
